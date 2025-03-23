c65da61719e0ccd3b66c20f4061f85998969154f
Tung Nguyen <tungnguyen@Tungs-MacBook-Air.local>
2025-03-23 10:01:59 -0400

    add sanity check

--- src/oakd_camera/check_connection.py
+++ src/oakd_camera/check_connection.py
@@ -1,13 +1,20 @@
 import depthai as dai
+from loguru import logger
 
-# Create a pipeline
-pipeline = dai.Pipeline()
 
-# Connect to the device
-with dai.Device(pipeline) as device:
-    # Print device information
-    print("Connected to device:")
-    print(f"Device name: {device.getDeviceName()}")
-    print(f"USB speed: {device.getUsbSpeed()}")
-    print(f"Connected cameras: {device.getConnectedCameras()}")
-    print(f"Available stereo pairs: {device.getAvailableStereoPairs()}")
+def main():
+    logger.info("Starting connection check...")
+    # Create a pipeline
+    pipeline = dai.Pipeline()
+
+    # Connect to the device
+    with dai.Device(pipeline) as device:
+        # Print device information
+        logger.info("Connected to device:")
+        logger.info(f"Device name: {device.getDeviceName()}")
+        logger.info(f"USB speed: {device.getUsbSpeed()}")
+        logger.info(f"Connected cameras: {device.getConnectedCameras()}")
+        logger.info(f"Available stereo pairs: {device.getAvailableStereoPairs()}")
+
+if __name__ == "__main__":
+    main()
